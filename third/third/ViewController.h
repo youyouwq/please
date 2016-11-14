7292c8c7803758910c5e42a3cc7118efb2b7e502
youyoumacmini3 <youyoumacmini3@youyoumacmini3demac-mini.local>
2016-11-14 21:17:22 +0800

    第一次---wang

---
+++ third/third/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  third
+//
+//  Created by youyoumacmini3 on 16/11/14.
+//  Copyright © 2016年 youyoumacmini3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ third/third/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  third
+//
+//  Created by youyoumacmini3 on 16/11/14.
+//  Copyright © 2016年 youyoumacmini3. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"lets made it");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
